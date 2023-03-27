c167afdf06aa51de68265409a11768087c670f11
{github HEHEEUN} <{heheeun13@gmail.com}>
2023-03-27 19:19:30 +0900

    Fix: 주석 추가 및 수정

--- cuk-lion-subject2/cuk-lion-subject2/main.swift
+++ cuk-lion-subject2/cuk-lion-subject2/main.swift
@@ -19,15 +19,15 @@ func makeNumSet() -> Set<Int> {
 
 func checkLottoNum(myLottoNumbers: Set<Int>) {
     var lottoNumSet = makeNumSet()
-    var sameNum: Set<Int> = Set<Int>()
+    var sameNumSet: Set<Int> = Set<Int>()
 
     // 겹치는 번호 교집합 추출
-    sameNum = myLottoNumbers.intersection(lottoNumSet)
+    sameNumSet = myLottoNumbers.intersection(lottoNumSet)
     
-    if(sameNum.count > 0){
-        var arraySameNum: [Int] = Array(sameNum)
+    if(sameNumSet.count > 0){
+        var arraySameNum: [Int] = Array(sameNumSet)
         arraySameNum.sort()
-        let sameNumString = arraySameNum.map { String($0) }.joined(separator: ", ") // set [] 제거 후 출력
+        let sameNumString = arraySameNum.map { String($0) }.joined(separator: ", ") // set 대괄호 제거하여 출력
         
         print("축하합니다! 겹치는 번호는 \(sameNumString)입니다!")
     }
@@ -36,50 +36,49 @@ func checkLottoNum(myLottoNumbers: Set<Int>) {
     }
 }
 
-func makeLottoDict(lottoRounds: Int, previousDict: [String: Set<Int>]) -> [String: Set<Int>] {
-
+func makeLottoDict(totalRounds: Int, previousDict: [String: Set<Int>]) -> [String: Set<Int>] {
+    
     var lottoNumDict: Dictionary<String, Set<Int>> = [String: Set<Int>]()
-    var lottoNum: Set<Int> = Set<Int>()
+    var lottoNumSet: Set<Int> = Set<Int>()
 
-    for i in previousDict.count..<lottoRounds {
-        lottoNum = makeNumSet()
-        lottoNumDict["\(i+1)회차"] = lottoNum
+    for i in previousDict.count..<totalRounds {
+        lottoNumSet = makeNumSet()
+        lottoNumDict["\(i+1)회차"] = lottoNumSet
     }
     return lottoNumDict
 }
 
-func appendLottoDict(lottoRounds: Int = 5) -> [String: Set<Int>] {
+func appendLottoDict(totalRounds: Int = 5) -> [String: Set<Int>] {
     var totalLottoDict: [String: Set<Int>] = [:]
-    var newLottoDict: [String: Set<Int>] = [:]
+    var newLottoDict: [String: Set<Int>]
     
-    newLottoDict = makeLottoDict(lottoRounds: lottoRounds, previousDict: totalLottoDict)
-    totalLottoDict.merge(newLottoDict, uniquingKeysWith: {(_, old) in old} ) // total + new
+    newLottoDict = makeLottoDict(totalRounds: totalRounds, previousDict: totalLottoDict)
+    totalLottoDict.merge(newLottoDict, uniquingKeysWith: {(_, old) in old} ) // total에 new추가 - total과 교집합은 유지
 
     return totalLottoDict
 }
 
-func showLottoAtRound(lottoRound:Int, lottoNumDict:[String: Set<Int>]) {
+func printLottoAtRound(atLottoRound:Int, lottoNumDict:[String: Set<Int>]) {
     // 로또 회차 없을 경우 예외처리
-    guard let lottoNumByRound = lottoNumDict["\(lottoRound)회차"] else {
-        print("\(lottoRound)회차의 로또 당첨 번호를 불러오는 데 실패했습니다.")
+    guard let lottoNumAtRound = lottoNumDict["\(atLottoRound)회차"] else {
+        print("\(atLottoRound)회차의 로또 당첨 번호를 불러오는 데 실패했습니다.")
         return
     }
-    var arraylottoNumByRound: [Int] = Array(lottoNumByRound)
-    arraylottoNumByRound.sort()
-    let lottoNumString = arraylottoNumByRound.map { String($0) }.joined(separator: ", ")
+    var arraylottoNumAtRound: [Int] = Array(lottoNumAtRound)
+    arraylottoNumAtRound.sort()
+    let lottoNumString = arraylottoNumAtRound.map { String($0) }.joined(separator: ", ") // set 대괄호 제거하여 출력
     
-    print("\(lottoRound)회차의 로또 당첨 번호는 \(lottoNumString) 입니다.")
+    print("\(atLottoRound)회차의 로또 당첨 번호는 \(lottoNumString) 입니다.")
 }
 
-func showLottoResult(lottoRound: Int)
-{
-    var lottoDict = appendLottoDict(lottoRounds: 5)
-    lottoDict = appendLottoDict(lottoRounds: 8)
-    showLottoAtRound(lottoRound: lottoRound, lottoNumDict: lottoDict)
+func showLottoResult(atLottoRound: Int) {
+    var lottoDict = appendLottoDict(totalRounds: 5)
+    lottoDict = appendLottoDict(totalRounds: 8)
+    printLottoAtRound(atLottoRound: atLottoRound, lottoNumDict: lottoDict)
 }
 
 let myLottoNumbers: Set<Int> = [46, 23, 12, 3, 5, 18]
 
 checkLottoNum(myLottoNumbers: myLottoNumbers)
 
-showLottoResult(lottoRound: 8)
+showLottoResult(atLottoRound: 8)
